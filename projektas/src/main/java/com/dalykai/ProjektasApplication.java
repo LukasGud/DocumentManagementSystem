1294178897cd79a98e9b11f201fa760f78f37792
Tomas <tomasa90@gmail.com>
2019-02-23 14:23:39 +0200

    // Added connection to mysql database _______________________ SQL code to generate required tables:
    table 1 :
    CREATE TABLE users (
    username varchar(50) not null,
    password varchar(50) not null,
    enabled tinyint(1) not null,
    
    primary key (username))
    
    _____
    
    INSERT INTO users VALUES
    ('john','{noop}test',1),
    ('mary','{noop}test',1),
    ('susan','{noop}test',1);
    
    table 2:
    
    CREATE TABLE authorities (
    username varchar(50) not null,
    authority varchar(50) not null,
    
    unique key authorities_idx_1 (username, authority),
    
    constraint authorities_ibfk_1
    foreign key (username)
    references users (username));
    
    ___
    
    INSERT INTO authorities values
    ('john','ROLE_EMPLOYEE'),
    ('mary','ROLE_EMPLOYEE'),
    ('susan','ROLE_EMPLOYEE'),
    ('mary','ROLE_MANAGER'),
    ('susan','ROLE_ADMIN')

--- projektas/src/main/java/com/dalykai/ProjektasApplication.java
+++ projektas/src/main/java/com/dalykai/ProjektasApplication.java
@@ -2,7 +2,7 @@ package com.dalykai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Just testing
 @SpringBootApplication
 public class ProjektasApplication {
 
