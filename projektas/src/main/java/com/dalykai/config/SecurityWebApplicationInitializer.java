a812a4c5990841e0baa947f34c55c9da34640417
Lukas <lukas.gudonis1989@gmail.com>
2019-02-25 19:48:12 +0200

    clean up

--- projektas/src/main/java/com/dalykai/config/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package com.dalykai.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer 
-						extends AbstractSecurityWebApplicationInitializer {
-
-}
