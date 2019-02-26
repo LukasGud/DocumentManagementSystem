1f8cf4daeb2ef47caf08f89367f65fbd3c5dfb63
Tomas <tomasa90@gmail.com>
2019-02-26 12:39:51 +0200

    Added an encryption of password when creating a new user and a @Bean for BCrypt

--- src/main/java/com/dalykai/config/LoginSecurityConfig.java
+++ src/main/java/com/dalykai/config/LoginSecurityConfig.java
@@ -1,12 +1,14 @@
 package com.dalykai.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.sql.DataSource;
@@ -65,6 +67,12 @@ public class LoginSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().exceptionHandling().accessDeniedPage("/accessDeniedPage");
 
     }
+
+	//BEAN for the bcrypt
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 }
 
