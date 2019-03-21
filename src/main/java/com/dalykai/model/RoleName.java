09695ce781f642834143054616ace173d3bc9015
Tomas <tomasa90@gmail.com>
2019-03-21 12:03:44 +0200

    Merging with json based login-sign up (3rd push)

---
+++ src/main/java/com/dalykai/model/RoleName.java
@@ -0,0 +1,6 @@
+package com.dalykai.model;
+
+public enum  RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/dalykai/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.dalykai.repository;
+
+
+import com.dalykai.model.Role;
+import com.dalykai.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
